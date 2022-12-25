8ce55f1d54c3f031fd24579f6bb60df47ff24ce0
Pranit Madhav Kulkarni <pkulkar2@binghamton.edu>
2022-12-24 18:25:31 -0800

    Complete List component exersice

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,24 +3,24 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        { name: 'Friend 1'},
-        { name: 'Friend 2'},
-        { name: 'Friend 3'},
-        { name: 'Friend 4'},
-        { name: 'Friend 5'},
-        { name: 'Friend 6'},
-        { name: 'Friend 7'},
-        { name: 'Friend 8'},
-        { name: 'Friend 9'},
-        { name: 'Friend 10'},
-        { name: 'Friend 11'},
-        { name: 'Friend 12'},
-        { name: 'Friend 13'},
-        { name: 'Friend 14'},
-        { name: 'Friend 15'},
-        { name: 'Friend 16'},
-        { name: 'Friend 17'},
-        { name: 'Friend 18'},
+        { name: 'Friend 1', age: 43},
+        { name: 'Friend 2', age: 51},
+        { name: 'Friend 3', age: 31},
+        { name: 'Friend 4', age: 21},
+        { name: 'Friend 5', age: 50},
+        { name: 'Friend 6', age: 10},
+        { name: 'Friend 7', age: 30},
+        { name: 'Friend 8', age: 24},
+        { name: 'Friend 9', age: 29},
+        { name: 'Friend 10', age: 27},
+        { name: 'Friend 11', age: 22},
+        { name: 'Friend 12', age: 21},
+        { name: 'Friend 13', age: 22},
+        { name: 'Friend 14', age: 23},
+        { name: 'Friend 15', age: 27},
+        { name: 'Friend 16', age: 26},
+        { name: 'Friend 17', age: 23},
+        { name: 'Friend 18', age: 22},
     ]
     return (
         /**
@@ -28,19 +28,18 @@ const ListScreen = () => {
          * to understand each properties mentioned
          */
         <FlatList
-        horizontal
-        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
         keyExtractor={(friend) => friend.name} 
         data={friends}
         renderItem={({item}) => {
-            return <Text style={styles.textStyle}>{item.name}</Text>
+            return <Text style={styles.textStyle}>{item.name} - {item.age}</Text>
         }}></FlatList>
     )
 }
 
 const styles = StyleSheet.create({
     textStyle: {
-        marginVertical: 50
+        marginVertical: 10
     }
 })
 
