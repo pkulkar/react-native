6955bae977bfeff4ec7ab2a50413c7ee841f5b09
Pranit Madhav Kulkarni <pkulkar2@binghamton.edu>
2022-12-24 18:01:38 -0800

    Display the list component

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
+    const friends = [
+        { name: 'Friend 1'},
+        { name: 'Friend 2'},
+        { name: 'Friend 3'},
+        { name: 'Friend 4'},
+        { name: 'Friend 5'},
+        { name: 'Friend 6'},
+    ]
     return (
-        <Text> List Screen </Text>
+        <FlatList data={friends}
+        renderItem={({item}) => {
+            //element === {item: { name: 'Friend 1'}, index:0 }
+            //item === { name: 'Friend 1'}
+            return <Text>{item.name}</Text>
+        }}></FlatList>
     )
 }
 
