2bcf1c57a80906cca2d4e620c92dd8bbf70fd523
Pranit Madhav Kulkarni <pkulkar2@binghamton.edu>
2022-12-25 17:00:15 -0800

    Check Button and Touchable Opacity components

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button 
+        title="Go to Components Screen"
+        onPress={()=> console.log('Button Pressed')}
+      />
+      <TouchableOpacity onPress={()=> console.log('List Pressed')}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
