16a302d4ee3a608392ac4c939c3592250e39d0c2
Pranit Madhav Kulkarni <pkulkar2@binghamton.edu>
2022-12-25 17:19:16 -0800

    Use buttons to navigate to different components (#3)
    * Check Button and Touchable Opacity components
    
    * Add navigation to buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button 
+        title="Go to Components Screen"
+        onPress={()=> navigation.navigate('Components')}
+      />
+      <Button 
+        title="Go to List Screen"
+        onPress={()=> navigation.navigate('List')}
+      />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
