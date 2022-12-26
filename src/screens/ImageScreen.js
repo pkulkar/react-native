943ef07c104d1026a63b337622d1dc2933c93707
Pranit Madhav Kulkarni <pkulkar2@binghamton.edu>
2022-12-25 17:27:01 -0800

    Add a new Image component and wire it to Home screen

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImageScreen = () => {
+
+}
+
+const styles = StyleSheet.create({})
+
+export default ImageScreen;
