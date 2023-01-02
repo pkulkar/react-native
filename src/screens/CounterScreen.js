04a0d0004676ef1486b848e9f344920dbc748a87
Pranit Madhav Kulkarni <pkulkar2@binghamton.edu>
2023-01-02 14:40:29 -0800

    Implement counter application

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
+    const [counter, setCounter] = useState(0); 
     return (
         <View>
-            <Text>Current Counter:</Text>
+            <Button 
+            title = "Increase Counter"
+            onPress={() => {
+                setCounter(counter + 1);
+            }}
+            />
+            <Button 
+            title = "Decrease Counter"
+            onPress={() => {
+                setCounter(counter - 1);
+            }}
+            />
+            <Text>Current Counter: {counter}</Text>
         </View>
     )
 }
